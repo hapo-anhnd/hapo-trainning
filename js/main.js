f76602bc9568614bc81966322abfad97d55957fd
Anh Dep Zoai <anh.nd@haposoft.com>
2020-07-28 11:54:02 +0700

    edit999

--- js/main.js
+++ js/main.js
@@ -6,3 +6,18 @@ $(document).ready(function(){
         $('.toggle-class').hide();
     });
 });
+
+function closeDiv(id) {
+    var closeBtn = document.getElementById(id);
+    closeBtn.style.display = "none";
+}
+
+function hidde_show(id) {
+    var chatBox = document.getElementById(id);
+    if(chatBox.style.display == "block") {
+        chatBox.style.display = "none";
+    } else {
+        chatBox.style.display = "block";
+    }
+}
+
